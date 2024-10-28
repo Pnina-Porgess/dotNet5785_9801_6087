35dcbdc34891e26e8db4d347fd746f2064cf6f07
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2024-10-28 17:20:55 +0200

    "Initial Code + Welcome functions"

---
+++ stage0/Program6087.cs
@@ -0,0 +1,20 @@
+﻿namespace stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            welcome9801();
+            welcome6087();
+            Console.ReadKey();
+
+        }
+        private static void welcome9801() { }
+        private static void welcome6087()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcome to my first console application", name);
+        }
+    }
+}
