7fe646d79ce16dda4262a0529e341426c96084b9
Sari-Katz <Hand4d17@mbjcomp.org.il>
2024-11-04 16:38:30 +0200

    I add class

---
+++ stage0/Program9801.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program9801
+    {
+        static partial void welcome9801()
+        {
+            Console.WriteLine("I am also here!");
+
+        }
+    }
+}
