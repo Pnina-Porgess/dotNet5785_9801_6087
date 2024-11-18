1bc2ef8f4ad4bd5fb5b22ab279a041e49db7aa2b
Sari-Katz <Hand4d17@mbjcomp.org.il>
2024-11-18 19:59:44 +0200

    add config

---
+++ DalList/Config.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dal;
+
+internal static class Config
+{
+
+    internal const int startCallId = 1000;
+    private static int nextCallId = startCallId;
+    internal static int NextCallId { get => nextCallId++; }
+
+    internal const int startAssignmentId = 1000;
+    private static int nextAssignmentId = startAssignmentId;
+    internal static int NextAssignmentId { get => nextAssignmentId++; }
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+    
+
+
+    internal static void Reset()
+    {
+        nextAssignmentId = startAssignmentId;
+        nextCallId = startCallId;
+        //...
+        Clock = DateTime.Now;
+        //...
+    }
+}
+
+
+}
