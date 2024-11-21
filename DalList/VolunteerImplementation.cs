e54920ee7e58a921b375341f7d38e7450fd98e14
Sari-Katz <Hand4d17@mbjcomp.org.il>
2024-11-21 21:59:37 +0200

    VolunteerImplementation

---
+++ DalList/VolunteerImplementation.cs
@@ -0,0 +1,40 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class VolunteerImplementation : IVolunteer
+{
+    public void Create(Volunteer item)
+    {
+        if(DataSource.Volunteers.Any(v=>v.Id == item.Id)) 
+         throw new NotImplementedException("An object of type Volunteer with such ID already exists.");
+        DataSource.Volunteers.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+       
+    }
+
+    public void DeleteAll()
+    {
+
+        throw new NotImplementedException();
+    }
+
+    public Volunteer? Read(int id)
+    {
+        return DataSource.Volunteers.Find(v => v.Id == id);
+    }
+
+    public List<Volunteer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+}
