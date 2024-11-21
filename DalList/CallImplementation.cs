bdd8b3b126fa447c256098a16d713ceb4b010811
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2024-11-21 22:12:22 +0200

    CallImplementation

--- DalList/CallImplementation.cs
+++ DalList/CallImplementation.cs
@@ -7,31 +7,40 @@ public class CallImplementation : ICall
 {
     public void Create(Call item)
     {
-        throw new NotImplementedException();
+
+        if (DataSource.Calls.Any(c => c?.Id == item.Id))
+            throw new NotImplementedException("Call with the same id already exists.");
+        DataSource.Calls.Add(item);
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        int removeCount = DataSource.Calls.RemoveAll(c => c?.Id == id);
+        if (removeCount == 0)
+            throw new NotImplementedException("Call not found");
     }
 
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        DataSource.Calls.Clear();
     }
 
     public Call? Read(int id)
     {
-        throw new NotImplementedException();
+        var call = DataSource.Calls.Find(c => c.Id == id);
+        return call;
     }
 
     public List<Call> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Call>(DataSource.Calls);
     }
 
     public void Update(Call item)
     {
-        throw new NotImplementedException();
+        int index = DataSource.Calls.FindIndex(c => c?.Id == item.Id);
+        if (index < 0)
+            throw new NotImplementedException("Call not found");
+        DataSource.Calls[index] = item;
     }
 }
