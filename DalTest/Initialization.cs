e71be85793e3c6465d0acb5c3ae109bd26462be7
Sari-Katz <Hand4d17@mbjcomp.org.il>
2024-11-23 21:21:47 +0200

    I add Initialization

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,31 @@
+﻿using DalApi;
+
+namespace DalTest;
+
+public static class Initialization
+{
+    private static IVolunteer? s_dalVolunteer; //stage 1
+    private static ICall? s_dalCall; //stage 1
+    private static IAssignment? s_dalAssignment; //stage 1
+    private static IConfig? s_dalConfig; //stage 1
+    private static readonly Random s_rand = new();
+    //...
+    private static void createVolunteers()
+    {
+
+    }
+    private static void createCalls()
+    {
+
+    }
+    private static void createAssignments()
+    {
+
+    }
+    private static void createConfigs()
+    {
+
+    }
+
+}
+
