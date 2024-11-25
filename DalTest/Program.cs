e5fb8e235997153f64abf29d3b8ba925dd73f48b
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2024-11-25 19:40:51 +0200

    ghugug

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dal;
 using DalApi;
 using DO;
+using System;
 
 namespace DalTest;
 
@@ -31,11 +32,174 @@ internal class Program
         Delete,
         DeleteAll
     }
-    private static void Create(string choice)
+    private static Volunteer CreateVolunteer(int id)
+    {
+        Console.Write("Enter your name");
+        string name=Console.ReadLine();
+        Console.Write("Enter your email");
+        string email = Console.ReadLine();
+        Console.Write("Enter your phone");
+        string phone = Console.ReadLine();
+        Console.Write("Enter your password");
+        string password = Console.ReadLine();
+        Console.Write("Enter  (1 for Manager, 2 for Volunteer, etc.): ");
+        Role Role = (Role)int.Parse(Console.ReadLine()!);
+        Console.Write("Is Active? (true/false): ");
+        bool Active = bool.Parse(Console.ReadLine()!);
+        Console.Write("Enter your address");
+        string address = Console.ReadLine();
+        Console.Write("Enter your Maximum Distance");
+        double MaximumDistance = double.Parse(Console.ReadLine()!);
+        Console.Write("Enter your Latitude");
+        double Latitude = double.Parse(Console.ReadLine()!);
+        Console.Write("Enter your Longitude");
+        double Longitude = double.Parse(Console.ReadLine()!);
+        Console.Write("Enter Distance Type  (1 for driving Distance, 2 for walking Distance,3 for Aerial Distance, etc.): ");
+        DistanceType DistanceType = (DistanceType)int.Parse(Console.ReadLine()!);
+        return new Volunteer(id, name, email, phone, Role, Active, DistanceType, MaximumDistance, password, address, Longitude, Latitude);
+    }
+    private static Call CreateCall(int id)
     {
 
+        Console.Write("Enter Call Type (1 for Type1, 2 for Type2, etc.): ");
+        TypeOfReading typeOfReading = (TypeOfReading)int.Parse(Console.ReadLine()!);
+        Console.Write("Enter Description of the problem");
+        string description= Console.ReadLine();
+        Console.Write("Enter your address");
+        string address = Console.ReadLine();
+        Console.Write("Enter your Latitude");
+        double Latitude = double.Parse(Console.ReadLine()!);
+        Console.Write("Enter your Longitude");
+        double Longitude = double.Parse(Console.ReadLine()!);
+        Console.Write("Enter Opening Time (YYYY-MM-DD HH:MM): ");
+        DateTime OpeningTime = DateTime.Parse(Console.ReadLine());
+        Console.Write("Enter Max Time Finish Calling (YYYY-MM-DD HH:MM): ");
+        DateTime MaxClosing = DateTime.Parse(Console.ReadLine());
+        return new Call(id, typeOfReading, description, address, Longitude, Latitude, OpeningTime, MaxClosing);
     }
 
+    private static Assignment CreateAssignment(int id)
+    {
+        Console.Write("Enter Volunteer ID: ");
+        int CallId = int.Parse(Console.ReadLine()!);
+        Console.Write("Enter Volunteer ID: ");
+        int volunteerId = int.Parse(Console.ReadLine()!);
+        Console.Write("Enter Type Of End Time : 1 for treated, 2 for Self Cancellation,3 for CancelingAnAdministrator,4 for CancellationHasExpired ");
+        TypeOfEndTime typeOfEndTime= (TypeOfEndTime)int.Parse(Console.ReadLine()!);
+        Console.Write("Enter Ending Time of Treatment ( YYYY-MM-DD HH:MM): ");
+        DateTime EndTime = DateTime.Parse(Console.ReadLine()!);
+        return new Assignment(id, CallId, volunteerId, typeOfEndTime, EndTime);
+    }
+        private static void Create(string choice)
+    {
+        Console.WriteLine("Enter your details");
+        Console.Write("Enter ID: ");
+        int yourId = int.Parse(Console.ReadLine()!);
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+                Volunteer Vol = CreateVolunteer(yourId);
+                s_dalVolunteer.Create(Vol);
+                break;
+            case "CallSubmenu":
+                Call Call = CreateCall(yourId);
+                s_dalCall.Create(Call);
+                break;
+            case "AssignmentSubmenu":
+                Assignment Ass = CreateAssignment(yourId);
+                s_dalAssignment.Create(Ass);
+                break;
+        }
+    }
+
+    private static void Update(string choice)
+    {
+        Console.WriteLine("Enter your details");
+        Console.Write("Enter ID: ");
+        int yourId = int.Parse(Console.ReadLine()!);
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+                Volunteer Vol = CreateVolunteer(yourId);
+                s_dalVolunteer.Update(Vol);
+                break;
+            case "CallSubmenu":
+                Call Call = CreateCall(yourId);
+                s_dalCall.Update(Call);
+                break;
+            case "AssignmentSubmenu":
+                Assignment Ass = CreateAssignment(yourId);
+                s_dalAssignment.Update(Ass);
+                break;
+        }
+    }
+
+    private static void Read(string choice)
+    {
+        Console.WriteLine("Enter ID: ");
+        int yourId = int.Parse(Console.ReadLine()!);
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+                s_dalVolunteer.Read(yourId);
+                break;
+            case "CallSubmenu":
+                s_dalCall.Delete(yourId);
+                break;
+            case "AssignmentSubmenu":
+                s_dalAssignment.Delete(yourId);
+                break;
+        }
+    }
+    private static void ReadAll(string choice)
+    {
+    
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+              Console.Write( s_dalVolunteer.ReadAll());
+                break;
+            case "CallSubmenu":
+                Console.Write(s_dalCall.ReadAll());
+                break;
+            case "AssignmentSubmenu":
+                Console.Write(s_dalAssignment.ReadAll());
+                break;
+        }
+    }
+    private static void Delete(string choice)
+    {
+        Console.WriteLine("Enter ID: ");
+        int yourId = int.Parse(Console.ReadLine()!);
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+                s_dalVolunteer.Delete(yourId);
+                break;
+            case "CallSubmenu":
+                s_dalCall.Delete(yourId);
+                break;
+            case "AssignmentSubmenu":
+                s_dalAssignment.Delete(yourId);
+                break;
+        }
+    }
+
+    private static void DeleteAll(string choice)
+    {
+        switch (choice)
+        {
+            case "VolunteerSubmenu":
+                s_dalVolunteer.DeleteAll();
+                break;
+            case "CallSubmenu":
+                s_dalCall.DeleteAll();
+                break;
+            case "AssignmentSubmenu":
+                s_dalAssignment.DeleteAll();
+                break;
+        }
+    }
     private static void EntityMenu(string choice)
     {
         Console.WriteLine("Enter a number");
@@ -53,9 +217,8 @@ internal class Program
                     break;
                 case SubMenu.Read:
                     Console.WriteLine("Enter Your ID");
-                    int YourId;
-                    if (!Enum.TryParse(Console.ReadLine(), out YourId)) throw new FormatException("Id is invalid!");
-                    Read(choice, yourId);
+
+                    Read(choice);
                     break;
                 case SubMenu.ReadAll:
                     ReadAll(choice);
@@ -101,12 +264,8 @@ internal class Program
           
                 break;
             case MainMenu.DisplayAllData:
-
-
                 break;
             case MainMenu.ConfigSubmenu:
-
-
             break;
         case MainMenu.ResetDatabase:
  
