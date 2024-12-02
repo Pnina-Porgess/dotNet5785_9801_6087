abe5b55c4b3ec9e1a041a793de1c98fcaba9ac1e
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2024-12-02 15:19:37 +0200

    פרקים 1,2,3

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace DalApi;
+public interface IDal
+{
+    IAssignment Assignment { get; }
+    IVolunteer Volunteer { get; }
+    ICall call { get; }
+    IConfig Config { get; }
+
+    void ResetDB();
+}
+
