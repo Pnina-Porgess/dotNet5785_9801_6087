ca49863a2a6730801ab0761de51e64546bd27f22
Sari-Katz <Hand4d17@mbjcomp.org.il>
2024-12-15 20:00:52 +0200

    DalXml

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,26 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+sealed public class DalXml : IDal
+{
+
+public IAssignment Assignment => throw new NotImplementedException();
+
+    public IVolunteer Volunteer => throw new NotImplementedException();
+
+    public ICall Call => throw new NotImplementedException();
+
+    public IConfig Config => throw new NotImplementedException();
+
+    public void ResetDB()
+    {
+        throw new NotImplementedException();
+    }
+}
+
