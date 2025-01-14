b217fa2c5c6b600fc34cff793278584c8a93992d
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2025-01-14 22:20:16 +0200

    i did 5 entities: VolunteerInList, Volunteer, OpenCallInList, ClosedCallInList, CallInProgress

--- BL/BO/OpenCallInList.cs
+++ BL/BO/OpenCallInList.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DO;
 
 namespace BO
 {
-    internal class OpenCallInList
+    public class OpenCallInList
     {
+       
+        public int Id { get; } // Call ID, unique for each call.
+
+        public TypeOfReading Type { get; } // Enum representing the type of the call.
+
+       
+        public string Description { get; } // A textual description of the call.
+
+       
+        public string FullAddress { get; } // The full address where the call is located.
+
+        
+        public DateTime OpenTime { get; } // The time when the call was opened.
+
+        
+        public DateTime? MaxEndTime { get; } // The deadline for completing the call (nullable).
+
+        public double DistanceFromVolunteer { get; } // Computed distance from the volunteer's location.
+
     }
+
 }
