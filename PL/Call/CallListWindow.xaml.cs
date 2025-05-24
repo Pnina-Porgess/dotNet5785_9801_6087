2b0baa4410a6be6a2cc2d1c1b3109b9973d8908d
Pnina-Porgess <Hand4d23@mbjcomp.org.il>
2025-05-25 02:54:50 +0300

    I made the calls list, we can to see the list on the screem now.

--- PL/Call/CallListWindow.xaml.cs
+++ PL/Call/CallListWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using BlApi;
+using Microsoft.VisualBasic;
 
 namespace PL.Call
 {
@@ -19,23 +13,41 @@ namespace PL.Call
     /// </summary>
     public partial class CallListWindow : Window
     {
-   
+        static readonly IBl s_bl = BlApi.Factory.Get();
         public CallListWindow()
         {
             InitializeComponent();
         }
-        public IEnumerable<BO.CallInList> CallList
+       public IEnumerable<BO.CallInList> CallList
+{
+    get { return (IEnumerable<BO.CallInList>)GetValue(CallListProperty); }
+    set { SetValue(CallListProperty, value); }
+}
+public static readonly DependencyProperty CallListProperty =
+    DependencyProperty.Register("CallList", typeof(IEnumerable<BO.CallInList>), typeof(CallListWindow), new PropertyMetadata(null));
+
+        public BO.CallField CallType { get; set; } = BO.CallField.AssignmentId;
+        private void queryCallList()
+           => CallList = (CallType == BO.CallField.None) ?
+               s_bl?.Call.GetCalls(null, null, null)! : s_bl?.Call.GetCalls(null, null, CallType)!;
+
+        private void callListObserver()
+            => queryCallList();
+ 
+private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            get { return (IEnumerable<BO.CallInList>)GetValue(CourseListProperty); }
-            set { SetValue(CourseListProperty, value); }
+            s_bl.Call.AddObserver(callListObserver);
+            queryCallList();
         }
-        public static readonly DependencyProperty CourseListProperty =
-            DependencyProperty.Register("CourseList", typeof(IEnumerable<BO.CallInList>), typeof(CallListWindow), new PropertyMetadata(null));
+
+        private void Window_Closed(object sender, EventArgs e)
+            => s_bl.Call.RemoveObserver(callListObserver);
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            queryCallList();
         }
+
 
     }
 }
--- PL/Enums.cs
+++ PL/Enums.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace PL;
 internal class CallFieldCollection : IEnumerable
 {
-    public BO.CallField Semester { get; set; } = BO.CallField.AssignmentId;
+    public BO.CallField CallType { get; set; } = BO.CallField.AssignmentId;
     static readonly IEnumerable<BO.CallField> s_enums =
 (Enum.GetValues(typeof(BO.CallField)) as IEnumerable<BO.CallField>)!;
 
