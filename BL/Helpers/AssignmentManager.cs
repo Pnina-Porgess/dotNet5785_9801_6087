b52b3e915255449a8b988bb313345b2a183df87d
Sari-Katz <Hand4d17@mbjcomp.org.il>
2025-07-01 14:50:33 +0300

    design VolunteerListWindow

--- BL/Helpers/AssignmentManager.cs
+++ BL/Helpers/AssignmentManager.cs
@@ -3,7 +3,9 @@ namespace Helpers;
 
 internal static class AssignmentManager
 {
+    lock (AdminManager.BlMutex) //stage 7
+    {
     private static IDal s_dal = Factory.Get; //stage 4
-    internal static ObserverManager Observers = new();
+    internal static ObserverManager Observers = new(); 
 
 }
